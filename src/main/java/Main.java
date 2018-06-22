fa6b33226aad745fc84afd2f4eecde18acbdb5a6
Damian Polchlopek <damian.polchlopek@gmail.com>
2018-06-22 21:56:39 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,52 @@
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private static Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        VBox vBox = new VBox();
+
+        StackPane root = new StackPane();
+        root.getChildren().add(vBox);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void stop() throws Exception {
+
+    }
+
+    @Override
+    public void init() throws Exception {
+
+    }
+
+    static public Stage getPrimaryStage() {
+        return Main.primaryStage;
+    }
+
+    private void setPrimaryStage(Stage stage) {
+        Main.primaryStage = stage;
+    }
+}
